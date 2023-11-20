cc0762d3af9bba976c6b169ce08a92d9ba9369c3
greypilgrim <jmcgrath245@gmail.com>
2023-11-19 23:52:50 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -10,40 +10,6 @@ public class Driver {
      */
     public static void main(String[] args) {
         AdminPanel adminControl = AdminPanel.getInstance();
-        
-        
-        
-        
-        
-        
-        
-        /*User jamesUser = new User("James");
-        User benUser = new User("Ben");
-
-        Follower mikeFollow = new Follower("Mike");
-        Follower billFollow = new Follower("Bill");
-
-        jamesUser.addObserver(mikeFollow);
-        jamesUser.addObserver(billFollow);
-        
-        benUser.addObserver(billFollow);
-
-        jamesUser.makePost("Hello, world!");
-        benUser.makePost("How are you doing?");
-
-        jamesUser.makePost("Goodbye, world!");
-        
-        jamesUser.printFollowers();
-        
-        User billUser = new User("Bill");
-        
-        billUser.addObserver(mikeFollow);
-        
-        
-        billUser.makePost("Bill's first post!");
-        
-        jamesUser.printPosts();
-        */
     }
     
 }
