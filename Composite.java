e884749a241dc9ebcd689a7b156225d13b9179fc
greypilgrim <jmcgrath245@gmail.com>
2023-12-11 12:58:55 -0800

    Update Composite.java
    Updated for Project 3

--- Composite.java
+++ Composite.java
@@ -1,9 +1,9 @@
-
-
 /**
  *
  * @author jmcgr
  */
 public interface Composite {
     public String getID();
+    public String creationTime();
+    public void setCreationTime(long creationTime);
 }
